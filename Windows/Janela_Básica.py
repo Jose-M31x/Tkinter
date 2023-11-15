334ed35842a7ef58aebd0dd9e4632d8778d60069
Joseph-DD2 <macedo31jose@hotmail.com>
2023-11-14 23:00:20 -0300

    Primeiro arquivo python
    Script WIP referente a uma janela básica sem elementos.

---
+++ Windows/Janela_Básica.py
@@ -0,0 +1,14 @@
+# Importação da biblioteca Tkinter
+from tkinter import *
+
+
+def main():
+    # Criação da instância da janela
+    application = Tk()
+
+    # Criação do loop principal
+    application.mainloop()
+
+
+if __name__ == '__main__':
+    main()
