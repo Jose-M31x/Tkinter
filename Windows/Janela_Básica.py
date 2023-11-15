f991bb37c0cad40a9180b2179778bafa670e615c
Joseph-DD2 <macedo31jose@hotmail.com>
2023-11-15 10:19:09 -0300

    Atualização do janela_basica.py
    Novos detalhes nos comentários.

--- Windows/Janela_Básica.py
+++ Windows/Janela_Básica.py
@@ -1,12 +1,14 @@
 # Importação da biblioteca Tkinter
+# Para intalar, basta usar o comando "pip install tk" no terminal
 from tkinter import *
 
 
 def main():
-    # Criação da instância da janela
+    # Criação da instância da janela usando a classe "TK"
     application = Tk()
 
     # Criação do loop principal
+    # "mainloop" é o método que coloca todo o conteúdo em exibição na janela
     application.mainloop()
 
 
